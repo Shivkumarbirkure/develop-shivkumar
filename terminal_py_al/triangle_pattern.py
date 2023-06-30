e7a7e5c72c5e0df1d9b16a3cab3ad95af05addc5
Shivkumar <shivkumarbirkure@gmail.com>
2023-06-30 20:15:17 +0530

    adding index.html

---
+++ terminal_py_al/triangle_pattern.py
@@ -0,0 +1,30 @@
+# def tri(n):
+
+#     k = n-1
+
+#     for i in range(0,n):
+#         for j in range(0,k):
+#             print(end="")
+
+#         k= k-1
+
+#         for j in range(0,i+1):
+#             print("* ",end =" ")
+#         print("\r")
+# n=5
+# tri(n)
+
+def new_(n):
+    k= n-1
+
+    for i in range(0,n):
+        for j in range(0,k):
+            print(end= " ")
+
+        k = k-1
+        for j in range(0,i+1):
+            print("* ",end= "")
+
+        print("\r")
+
+new_(5)
